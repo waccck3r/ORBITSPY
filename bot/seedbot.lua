0b65fa05f9aad3b1a22571e00837356550461376
waccck3r <waccck3r@gmail.com>
2016-02-27 20:39:41 +0330

    Update seedbot.lua

--- bot/seedbot.lua
+++ bot/seedbot.lua
@@ -226,7 +226,7 @@ function create_config( )
     "leave_ban",
     "admin"
     },
-    sudo_users = {153198958},--Sudo users
+    sudo_users = {174717558},--Sudo users
     disabled_channels = {},
     moderation = {data = 'data/moderation.json'},
     about_text = [[Teleseed v2 - Open Source
